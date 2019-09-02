fed1fd9ee57d67c0194300e45141fb8a25d47835
G H <gh@Gs-MacBook-Pro.local>
2019-09-02 12:43:19 -0700

    mvp

--- js/index.js
+++ js/index.js
@@ -62,3 +62,37 @@ let nav = document.querySelector('nav')
 nav.prepend(home);
 nav.appendChild(contact);
 
+
+//Cta
+let title = document.querySelector(".cta h1");
+let btn = document.querySelector(".cta button");
+let ctaImg = document.querySelector(".cta img");
+
+title.textContent = siteContent.cta.h1;
+btn.textContent = siteContent.cta.button;
+ctaImg.setAttribute('src', siteContent.cta["img-src"]);
+
+ title.style.width = "240px";
+
+//Middle
+let midheadings = document.querySelectorAll(".text-content h4");
+let midtext = document.querySelectorAll(".text-content p");
+let midImg = document.querySelector(".middle-img");
+midImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+//Middle heading
+midheadings[0].textContent = siteContent["main-content"]["features-h4"];
+midheadings[1].textContent = siteContent["main-content"]["about-h4"];
+midheadings[2].textContent = siteContent["main-content"]["services-h4"];
+midheadings[3].textContent = siteContent["main-content"]["product-h4"];
+midheadings[4].textContent = siteContent["main-content"]["vision-h4"];
+
+//Middle text
+midtext[0].textContent = siteContent["main-content"]["features-content"];
+midtext[1].textContent = siteContent["main-content"]["about-content"];
+midtext[2].textContent = siteContent["main-content"]["services-content"];
+midtext[3].textContent = siteContent["main-content"]["product-content"];
+midtext[4].textContent = siteContent["main-content"]["vision-content"];
+
+
+
